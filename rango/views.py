1101b6baec4cb2582463ffef5c5928e4b90ea4b1
Eli Agbenu <eli.agbenu@gh4hub.com>
2015-09-01 11:39:32 +0000

    added the django redux registration package

--- rango/views.py
+++ rango/views.py
@@ -7,19 +7,46 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import logout
+from datetime import datetime
 
 # Create your views here.
 #def index(request):
 #    return HttpResponse("Rango says hey there world!")
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {'categories': category_list}
+    context_dict = {'categories': category_list, 'pages': page_list}
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
 
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
@@ -188,6 +215,13 @@ def user_login(request):
         return render(request, 'rango/login.html', {})
 
 def register(request):
+
+    """
+    testing for cookies
+    if request.session.test_cookie_worked():
+        print ">>>> TEST COOKIE WORKED!"
+        request.session.delete_test_cookie()
+    """
 
     # A boolean value for telling the template whether the registration was successful.
     # Set to False initially. Code changes value to True when registration succeeds.
--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -45,7 +45,7 @@ LOCAL_APPS=(
 )
 
 THIRD_PARTY_APPS=(
-
+    'registration', # add in the registration package
 )
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
@@ -65,7 +65,14 @@ PASSWORD_HASHERS = (
 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 )
 
-LOGIN_URL = '/rango/login/'
+#LOGIN_URL = '/rango/login/'
+
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                                                # and are trying to access pages requiring authentication
 
 ROOT_URLCONF = 'tango_with_django.urls'
 
